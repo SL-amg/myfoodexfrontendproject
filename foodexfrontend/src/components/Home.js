3f30e4a8ac0878f4a279feec82af11912c4683f6
Abdullah Abbas <sl_amg@msn.com>
2024-12-13 23:31:47 +0300

    made sample div Home

--- foodexfrontend/src/components/Home.js
+++ foodexfrontend/src/components/Home.js
@@ -1,20 +1,30 @@
-import React from 'react'
-import "../components/CSS files/Home.css"
+import React from "react";
+import "../components/CSS files/Home.css";
+import Arabicpic from "/Users/slamg/Desktop/Development/myfoodexfrontendproject/foodexfrontend/src/images/Arabic.png";
+import Desertspic from "/Users/slamg/Desktop/Development/myfoodexfrontendproject/foodexfrontend/src/images/Deserts.png";
+import Healthypic from "/Users/slamg/Desktop/Development/myfoodexfrontendproject/foodexfrontend/src/images/Healthy.png";
+import Kuwaitipic from "/Users/slamg/Desktop/Development/myfoodexfrontendproject/foodexfrontend/src/images/Kuwaiti.png";
 
 
 const Home = () => {
   return (
-   <>
-   {/* Home Entry Bar */}
-   <div className='enteryBar'> 
-   <a className='textBar'>Simple recipes made for</a> <h1 className='ballet-font'> real actual everday Life</h1> 
-   </div>
-    {/* Samples of what we offer Div */}
-   <div>
-    
-   </div>
-   </>
-  )
-}
+    <>
+      {/* Home Entry Bar */}
+      <div className="enteryBar">
+        <a className="textBar">Simple recipes made for</a>{" "}
+        <h1 className="ballet-font"> real actual everday Life</h1>
+      </div>
+      {/* Samples of what we offer Div */}
+      <div className="sampleDiv">
+        <img src={Arabicpic} className="imagesReSize" alt="rigister Logo" />
+        <img src={Desertspic} className="imagesReSize" alt="rigister Logo" />
+        <img src={Healthypic} className="imagesReSize" alt="rigister Logo" />
+        <img src={Kuwaitipic} className="imagesReSize" alt="rigister Logo" />
+      </div>
+       {/* Catogaries Div */}
+       
+    </>
+  );
+};
 
-export default Home
+export default Home;
