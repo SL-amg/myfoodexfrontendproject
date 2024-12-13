dbc7cc316f500e255683da1f29c51d61d73d1d47
Abdullah Abbas <sl_amg@msn.com>
2024-12-14 00:50:05 +0300

    finished my catogires main page

--- foodexfrontend/src/components/Home.js
+++ foodexfrontend/src/components/Home.js
@@ -5,13 +5,20 @@ import Desertspic from "/Users/slamg/Desktop/Development/myfoodexfrontendproject
 import Healthypic from "/Users/slamg/Desktop/Development/myfoodexfrontendproject/foodexfrontend/src/images/Healthy.png";
 import Kuwaitipic from "/Users/slamg/Desktop/Development/myfoodexfrontendproject/foodexfrontend/src/images/Kuwaiti.png";
 
+import asianfood from "/Users/slamg/Desktop/Development/myfoodexfrontendproject/foodexfrontend/src/images/asianfood.avif";
+import desertfood from "/Users/slamg/Desktop/Development/myfoodexfrontendproject/foodexfrontend/src/images/desertsfood.avif";
+import fastfood from "/Users/slamg/Desktop/Development/myfoodexfrontendproject/foodexfrontend/src/images/fastfoodpic.jpg";
+import healthyfood from "/Users/slamg/Desktop/Development/myfoodexfrontendproject/foodexfrontend/src/images/healthyfood.png";
+import kuwaitifood from "/Users/slamg/Desktop/Development/myfoodexfrontendproject/foodexfrontend/src/images/kuwaitifood.jpg";
+import arabicfood from "/Users/slamg/Desktop/Development/myfoodexfrontendproject/foodexfrontend/src/images/labaniesfood.avif";
+import turkishfood from "/Users/slamg/Desktop/Development/myfoodexfrontendproject/foodexfrontend/src/images/turkishfood.webp";
 
 const Home = () => {
   return (
     <>
       {/* Home Entry Bar */}
       <div className="enteryBar">
-        <a className="textBar">Simple recipes made for</a>{" "}
+        <p className="textBar">Simple recipes made for</p>
         <h1 className="ballet-font"> real actual everday Life</h1>
       </div>
       {/* Samples of what we offer Div */}
@@ -21,8 +28,47 @@ const Home = () => {
         <img src={Healthypic} className="imagesReSize" alt="rigister Logo" />
         <img src={Kuwaitipic} className="imagesReSize" alt="rigister Logo" />
       </div>
-       {/* Catogaries Div */}
-       
+      {/* Catogaries Div */}
+      <div>
+        <div className="categoriesBar">
+          <h1 className="ballet-font">Our Catogaries</h1>
+        </div>
+        {/* Catogaries list Div */}
+        <div className="categorySelector">
+          <div className="oneCatigoreyDiv">
+            <img src={asianfood} className="oneImageSizing" alt="Asian" />
+            <p>Asian</p>
+          </div>
+          <div className="oneCatigoreyDiv">
+            <img src={desertfood} className="oneImageSizing" alt="Asian" />
+            <p>Deserts</p>
+          </div>
+          <div className="oneCatigoreyDiv">
+            <img src={fastfood} className="oneImageSizing" alt="Asian" />
+            <p>Fast Food</p>
+          </div>
+          <div className="oneCatigoreyDiv">
+            <img src={healthyfood} className="oneImageSizing" alt="Healthy" />
+            <p>Healthy</p>
+          </div>
+          <div className="oneCatigoreyDiv">
+            <img src={kuwaitifood} className="oneImageSizing" alt="Kuwaiti" />
+            <p>Kuwaiti</p>
+          </div>
+          <div className="oneCatigoreyDiv">
+            <img src={arabicfood} className="oneImageSizing" alt="Arabic" />
+            <p>Arabic</p>
+          </div>
+          <div className="oneCatigoreyDiv">
+            <img src={turkishfood} className="oneImageSizing" alt="Turkish" />
+            <p>Turkish</p>
+          </div>
+        </div>
+        {/* View Recipies Page Sample Div */}
+        <div>
+          
+        </div>
+      </div>
     </>
   );
 };
