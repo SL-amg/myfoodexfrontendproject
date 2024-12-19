272ecb4318fc1e157bc4790f8d80499f01472e64
Abdullah Abbas <sl_amg@msn.com>
2024-12-19 13:21:56 +0300

    added image upload

--- foodexfrontend/src/components/CreateIngredienTestFile.js
+++ foodexfrontend/src/components/CreateIngredienTestFile.js
@@ -1,82 +1,82 @@
-import React from 'react'
-import { Formik, Form, Field } from "formik";
-import { NavLink, useNavigate } from "react-router-dom";
-import { addIngredient } from "../api/users";
-import { useQuery, useMutation } from "@tanstack/react-query";
-import { useState } from "react";
+// import React from 'react'
+// import { Formik, Form, Field } from "formik";
+// import { NavLink, useNavigate } from "react-router-dom";
+// import { addIngredient } from "../api/users";
+// import { useQuery, useMutation } from "@tanstack/react-query";
+// import { useState } from "react";
 
-const CreateIngredient = () => {
+// const CreateIngredient = () => {
 
-    const navigate = useNavigate();
+//     const navigate = useNavigate();
 
-    const mutation = useMutation({
-      mutationFn: (newIngredient) => addIngredient(newIngredient),
-      onSuccess: () => {
-        navigate("/ingredientList");
-        const refreshPage = () => {
-            navigate(0);
-        }},
-    });
+//     const mutation = useMutation({
+//       mutationFn: (newIngredient) => addIngredient(newIngredient),
+//       onSuccess: () => {
+//         navigate("/ingredientList");
+//         const refreshPage = () => {
+//             navigate(0);
+//         }},
+//     });
   
-    function submit(values) {
-      console.log(values);
-      mutation.mutate(values);
-    }
+//     function submit(values) {
+//       console.log(values);
+//       mutation.mutate(values);
+//     }
 
 
-  return (
-    <>
-    <div>
-            {/* Formik and rigister div*/}
-            <Formik
-              initialValues={{
-                name: "",
-                scale: "",
-                amount: "",
-              }}
-              onSubmit={submit}
-            >
-              <Form>
-                <div>
-                  <div>
-                    <h4>Name</h4>
-                    <Field
-                      placeholder="name"
-                      className="barStyle"
-                      as="input"
-                      name="name"
-                      type="text"
-                    />
-                  </div>
-                  <div>
-                    <h4>scale</h4>
-                    <Field
-                      placeholder="scale"
-                      className="barStyle"
-                      as="input"
-                      name="scale"
-                      type="text"
-                    />
-                  </div>
-                  <div>
-                    <h4>amount</h4>
-                    <Field
-                      placeholder="amount"
-                      className="barStyle"
-                      as="input"
-                      name="amount"
-                      type="text"
-                    />
-                  </div>
-                  <button className="" type="submit">
-                    Create Ingredient
-                  </button>
-                </div>
-              </Form>
-            </Formik>
-   </div>
-   </>
-  )
-}
+//   return (
+//     <>
+//     <div>
+//             {/* Formik and rigister div*/}
+//             <Formik
+//               initialValues={{
+//                 name: "",
+//                 scale: "",
+//                 amount: "",
+//               }}
+//               onSubmit={submit}
+//             >
+//               <Form>
+//                 <div>
+//                   <div>
+//                     <h4>Name</h4>
+//                     <Field
+//                       placeholder="name"
+//                       className="barStyle"
+//                       as="input"
+//                       name="name"
+//                       type="text"
+//                     />
+//                   </div>
+//                   <div>
+//                     <h4>scale</h4>
+//                     <Field
+//                       placeholder="scale"
+//                       className="barStyle"
+//                       as="input"
+//                       name="scale"
+//                       type="text"
+//                     />
+//                   </div>
+//                   <div>
+//                     <h4>amount</h4>
+//                     <Field
+//                       placeholder="amount"
+//                       className="barStyle"
+//                       as="input"
+//                       name="amount"
+//                       type="text"
+//                     />
+//                   </div>
+//                   <button className="" type="submit">
+//                     Create Ingredient
+//                   </button>
+//                 </div>
+//               </Form>
+//             </Formik>
+//    </div>
+//    </>
+//   )
+// }
 
-export default CreateIngredient
+// export default CreateIngredient
